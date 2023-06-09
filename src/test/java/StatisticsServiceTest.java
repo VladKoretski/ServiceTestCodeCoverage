f58a035133641e19eda476ac1fd08e96ec118f51
Vladko1973 <vpk1973@yandex.ru>
2023-06-09 18:51:36 +0300

    third commit

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -16,15 +16,15 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void findMaxIfAnyMonthIncome() {
-        StatisticsService service = new StatisticsService();
+  //  @Test
+  //  void findMaxIfAnyMonthIncome() {
+  //      StatisticsService service = new StatisticsService();
+//
+  //      long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 15, 11, 12};
+  //      long expected = 15;
+//
+    //    long actual = service.findMax(incomesInBillions);
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 15, 11, 12};
-        long expected = 15;
-
-        long actual = service.findMax(incomesInBillions);
-
-        Assertions.assertEquals(expected, actual);
-    }
+  //      Assertions.assertEquals(expected, actual);
+ //   }
 }
